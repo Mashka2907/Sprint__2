0492519129b7c87c385cd8aa34c1300f3b59e8e9
Mashka <krymzova.m@mail.ru>
2025-05-25 09:55:40 +0300

    Исправление 3 задания

--- task_3.py
+++ task_3.py
@@ -5,12 +5,12 @@ class PointsForPlace:
         self.points = 0
 
     def get_points_for_place(self, place):
-        if place > 100:
+        if int(place) > 100:
             return 'Баллы начисляются только первым 100 участникам'
-        elif place < 1:
+        elif int(place) < 1:
             return 'Спортсмен не может занять нулевое или отрицательное место'
         else:
-            self.points = 101 - place
+            self.points = 101 - int(place)
             return self.points
 
 class PointsForMeters:
@@ -20,17 +20,17 @@ class PointsForMeters:
         self.points = 0
 
     def get_points_for_meters(self, meters):
-        if meters < 0:
+        if float(meters) < 0:
             return 'Количество метров не может быть отрицательным'
         else:
-            self.points = meters * 0.5
+            self.points = float(meters) * 0.5
             return self.points
 
 class TotalPoints(PointsForPlace, PointsForMeters):
     """Вычисляет общую сумму очков, складывая очки за место и очки за метры"""
 
     def get_total_points(self, meters, place):
-        total = float(self.get_points_for_place(place)) + float(self.get_points_for_meters(meters))
+        total = self.get_points_for_place(place) + self.get_points_for_meters(meters)
         return total
 
 
